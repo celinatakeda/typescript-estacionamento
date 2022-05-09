48206949eb29c53f046e63f60bfd18c14f072cf7
Celina Takeda <takedacarina@gmail.com>
2022-05-09 10:30:34 -0300

    commit

--- script.ts
+++ script.ts
@@ -1,11 +1,28 @@
 interface Veiculo {
     nome: string;
     placa: string;
-    entrada: Date;
+    entrada: Date | string;
+    clientId?: string;
+}
+
+interface Pessoa {
+    nome: string;
+    cpf: string;
+}
+
+interface Cliente extends Pessoa {
+    veiculos: Veiculo[];
 }
 
 (function () { 
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function calcTempo(mil: number) {
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) / 1000);
+
+        return `${min} e ${sec}`;
+    }
 
     function patio() {
         function ler(): Veiculo[] {
@@ -16,7 +33,7 @@ interface Veiculo {
             localStorage.setItem('patio', JSON.stringify(veiculos));
         }
 
-        function adicionar(veiculo: Veiculo, salva?: boolean) {
+        function adicionar(veiculo: Veiculo & { cupom?: string} , salva?: boolean) {
             const row = document.createElement("tr");
 
             row.innerHTML = `
@@ -28,13 +45,27 @@ interface Veiculo {
                 </td>
             `;
 
+            row.querySelector(".delete")?.addEventListener("click", function () {
+                remover(this.dataset.placa);
+            });
+
             $("#patio")?.appendChild(row);
 
             if(salva) salvar([...ler(), veiculo])
         }
 
-        function remover() {}
+        function remover(placa: string) {
+            const { entrada, nome } = ler().find(veiculo => veiculo.placa === placa);
 
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+
+            if(!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar?`)
+            ) 
+            return;
+
+            salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+            render();
+        }
 
         function render() {
             $("#patio")!.innerHTML = "";
@@ -59,6 +90,6 @@ interface Veiculo {
             return;
         }
 
-        patio().adicionar({ nome, placa, entrada: new Date() }, true);
+        patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true);
     })
 })();
